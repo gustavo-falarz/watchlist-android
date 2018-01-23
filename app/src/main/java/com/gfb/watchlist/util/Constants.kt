8579c906a2c7ce7940056632ea9435f92920f034
Gustavo <gustavo.falarz@gmail.com>
2018-01-23 09:37:21 -0200

    Cleaning up the code

--- app/src/main/java/com/gfb/watchlist/util/Constants.kt
+++ app/src/main/java/com/gfb/watchlist/util/Constants.kt
@@ -3,8 +3,9 @@ package com.gfb.watchlist.util
 /**
  * Created by Gustavo on 1/23/2018.
  */
+
 object Constants {
-    val TYPE_MOVIE = "movies"
+    val TYPE_MOVIE = "movie"
     val TYPE_SERIES = "series"
     val TRANSITION_KEY_CONTENT = "content"
 }
