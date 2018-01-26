bef991486d4e8960ef3f61daa0810b3be7d0d3c2
Gustavo <gustavo.falarz@gmail.com>
2018-01-26 13:31:16 -0200

    Google login

--- app/src/main/java/com/gfb/watchlist/WatchlistApplication.kt
+++ app/src/main/java/com/gfb/watchlist/WatchlistApplication.kt
@@ -2,16 +2,14 @@ package com.gfb.watchlist
 
 import android.app.Application
 import com.chibatching.kotpref.Kotpref
-import com.google.firebase.FirebaseApp
 
 /**
  * Created by Gustavo on 12/26/2017.
  */
-class WatchlistApplication: Application() {
+class WatchlistApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Kotpref.init(applicationContext)
-        FirebaseApp.initializeApp(applicationContext)
     }
 }
