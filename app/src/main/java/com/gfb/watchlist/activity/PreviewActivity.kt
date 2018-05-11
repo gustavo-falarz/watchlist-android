16ea6e8b646cf86172996f5c26f6748c299eb835
gustavo.batista <gustavo.batista@tivit.com>
2018-05-11 15:46:46 -0300

    Added preview to items on the list

---
+++ app/src/main/java/com/gfb/watchlist/activity/PreviewActivity.kt
@@ -0,0 +1,12 @@
+package com.gfb.watchlist.activity
+
+import android.app.Activity
+import android.os.Bundle
+
+class PreviewActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
